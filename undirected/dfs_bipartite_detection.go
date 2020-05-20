35e1ff591c154ba4ff6dd23582b4c77675740b62
Soul-Mate <markhub.cn@outlook.com>
2020-05-20 17:31:51 +0800

    dfs bipartite detection some code

--- undirected/dfs_bipartite_detection.go
+++ undirected/dfs_bipartite_detection.go
@@ -1,1 +1,23 @@
 package undirected
+
+type BipartiteDetection struct {
+	g           Graph
+	visited     []bool
+	isBipartite bool
+}
+
+func NewBipartiteDetection(g Graph) *BipartiteDetection {
+	b := new(BipartiteDetection)
+	b.g = g
+	b.isBipartite = true
+	b.visited = make([]bool, b.g.V())
+
+	for i := 0; i < b.g.V(); i++ {
+		if !b.visited[i] && b.dfs(i) {
+			b.isBipartite = true
+			break
+		}
+	}
+
+	return b
+}
